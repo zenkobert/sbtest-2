9dfcd4b9baf9992e29e8f4a7eaa84584d244cce6
Albert Zenko <zenkobert@gmail.com>
2021-09-04 22:44:14 +0700

    Implement MovieUsecase

---
+++ usecase/movie.go
@@ -0,0 +1,23 @@
+package movie
+
+import (
+	model "github.com/zenkobert/sbtest-2/domain"
+)
+
+type movieUsecase struct {
+	MovieRepo model.MovieRepository
+}
+
+func NewMovieUsecase(movieRepo model.MovieRepository) model.MovieUsecase {
+	return &movieUsecase{
+		MovieRepo: movieRepo,
+	}
+}
+
+func (usecase *movieUsecase) SearchMovies(title string, page uint32) (result *model.MovieSearch, err error) {
+	return usecase.MovieRepo.SearchMovies(title, page)
+}
+
+func (usecase *movieUsecase) GetMovieDetailByID(id string) (detail *model.MovieDetail, err error) {
+	return usecase.MovieRepo.GetMovieDetailByID(id)
+}
