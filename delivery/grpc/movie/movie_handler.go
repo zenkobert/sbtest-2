aad2a54d4207f2b1d9448adfb50dc2ad7ef838a7
Albert Zenko <zenkobert@gmail.com>
2021-09-05 12:45:20 +0700

    handle possible errors

--- delivery/grpc/movie/movie_handler.go
+++ delivery/grpc/movie/movie_handler.go
@@ -2,10 +2,18 @@ package movie
 
 import (
 	context "context"
+	"net/url"
+	"strconv"
+	"strings"
 
 	model "github.com/zenkobert/sbtest-2/domain"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+)
+
+var (
+	incorrectImdbIDError = status.Error(codes.InvalidArgument, "incorrect IMDB ID")
+	movieNotFoundError   = status.Error(codes.NotFound, "movie not found")
 )
 
 type movieServer struct {
@@ -19,20 +27,55 @@ func NewMovieServer(movieusecase model.MovieUsecase) SearchMovieServer {
 }
 
 func (serv *movieServer) SearchMovie(ctx context.Context, req *SearchMovieRequest) (resp *SearchMovieResponse, err error) {
+	if req.Pagination <= 0 {
+		req.Pagination = 1
+	}
+
+	req.Searchword = url.QueryEscape(req.Searchword)
+
 	movieSearch, err := serv.MovieUsecase.SearchMovies(req.Searchword, uint32(req.Pagination))
 	if err != nil {
-		return resp, status.Error(codes.Internal, "Oops, something happened")
+		return resp, status.Error(codes.Internal, err.Error())
+	}
+
+	if movieSearch.Error != "" {
+		return resp, movieNotFoundError
 	}
 
 	return serv.convertMovieSearchToRPCResponse(movieSearch), nil
 }
+
 func (serv *movieServer) GetMovieDetail(ctx context.Context, req *GetMovieDetailRequest) (resp *GetMovieDetailResponse, err error) {
+	err = validateImdbID(req.Id)
+	if err != nil {
+		return resp, err
+	}
+
 	detail, err := serv.MovieUsecase.GetMovieDetailByID(req.Id)
 	if err != nil {
-		return resp, status.Error(codes.Internal, "Oops, something happened")
+		return resp, status.Error(codes.Internal, err.Error())
+	}
+
+	if detail.Error != "" {
+		return resp, movieNotFoundError
 	}
 
 	return serv.convertMovieDetailToRPCResponse(detail), nil
+}
+
+func validateImdbID(id string) error {
+	prefixIdx := strings.Index(id, "tt")
+	if prefixIdx < 0 {
+		return incorrectImdbIDError
+	}
+
+	id = strings.ReplaceAll(id, "tt", "")
+	_, err := strconv.Atoi(id)
+	if err != nil {
+		return incorrectImdbIDError
+	}
+
+	return nil
 }
 
 func (serv *movieServer) convertMovieSearchToRPCResponse(m *model.MovieSearch) (r *SearchMovieResponse) {
