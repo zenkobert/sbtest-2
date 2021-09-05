547eb483fd2a872234fc381ffb34620fe3d5ee71
Albert Zenko <zenkobert@gmail.com>
2021-09-05 10:54:02 +0700

    rename service

--- delivery/grpc/movie/movie_grpc.pb.go
+++ delivery/grpc/movie/movie_grpc.pb.go
@@ -14,120 +14,120 @@ import (
 // Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
-// SearchMovieServiceClient is the client API for SearchMovieService service.
+// SearchMovieClient is the client API for SearchMovie service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type SearchMovieServiceClient interface {
+type SearchMovieClient interface {
 	SearchMovie(ctx context.Context, in *SearchMovieRequest, opts ...grpc.CallOption) (*SearchMovieResponse, error)
 	GetMovieDetail(ctx context.Context, in *GetMovieDetailRequest, opts ...grpc.CallOption) (*GetMovieDetailResponse, error)
 }
 
-type searchMovieServiceClient struct {
+type searchMovieClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewSearchMovieServiceClient(cc grpc.ClientConnInterface) SearchMovieServiceClient {
-	return &searchMovieServiceClient{cc}
+func NewSearchMovieClient(cc grpc.ClientConnInterface) SearchMovieClient {
+	return &searchMovieClient{cc}
 }
 
-func (c *searchMovieServiceClient) SearchMovie(ctx context.Context, in *SearchMovieRequest, opts ...grpc.CallOption) (*SearchMovieResponse, error) {
+func (c *searchMovieClient) SearchMovie(ctx context.Context, in *SearchMovieRequest, opts ...grpc.CallOption) (*SearchMovieResponse, error) {
 	out := new(SearchMovieResponse)
-	err := c.cc.Invoke(ctx, "/movie.SearchMovieService/SearchMovie", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/movie.SearchMovie/SearchMovie", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *searchMovieServiceClient) GetMovieDetail(ctx context.Context, in *GetMovieDetailRequest, opts ...grpc.CallOption) (*GetMovieDetailResponse, error) {
+func (c *searchMovieClient) GetMovieDetail(ctx context.Context, in *GetMovieDetailRequest, opts ...grpc.CallOption) (*GetMovieDetailResponse, error) {
 	out := new(GetMovieDetailResponse)
-	err := c.cc.Invoke(ctx, "/movie.SearchMovieService/GetMovieDetail", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/movie.SearchMovie/GetMovieDetail", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// SearchMovieServiceServer is the server API for SearchMovieService service.
-// All implementations should embed UnimplementedSearchMovieServiceServer
+// SearchMovieServer is the server API for SearchMovie service.
+// All implementations should embed UnimplementedSearchMovieServer
 // for forward compatibility
-type SearchMovieServiceServer interface {
+type SearchMovieServer interface {
 	SearchMovie(context.Context, *SearchMovieRequest) (*SearchMovieResponse, error)
 	GetMovieDetail(context.Context, *GetMovieDetailRequest) (*GetMovieDetailResponse, error)
 }
 
-// UnimplementedSearchMovieServiceServer should be embedded to have forward compatible implementations.
-type UnimplementedSearchMovieServiceServer struct {
+// UnimplementedSearchMovieServer should be embedded to have forward compatible implementations.
+type UnimplementedSearchMovieServer struct {
 }
 
-func (UnimplementedSearchMovieServiceServer) SearchMovie(context.Context, *SearchMovieRequest) (*SearchMovieResponse, error) {
+func (UnimplementedSearchMovieServer) SearchMovie(context.Context, *SearchMovieRequest) (*SearchMovieResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method SearchMovie not implemented")
 }
-func (UnimplementedSearchMovieServiceServer) GetMovieDetail(context.Context, *GetMovieDetailRequest) (*GetMovieDetailResponse, error) {
+func (UnimplementedSearchMovieServer) GetMovieDetail(context.Context, *GetMovieDetailRequest) (*GetMovieDetailResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetMovieDetail not implemented")
 }
 
-// UnsafeSearchMovieServiceServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to SearchMovieServiceServer will
+// UnsafeSearchMovieServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to SearchMovieServer will
 // result in compilation errors.
-type UnsafeSearchMovieServiceServer interface {
-	mustEmbedUnimplementedSearchMovieServiceServer()
+type UnsafeSearchMovieServer interface {
+	mustEmbedUnimplementedSearchMovieServer()
 }
 
-func RegisterSearchMovieServiceServer(s grpc.ServiceRegistrar, srv SearchMovieServiceServer) {
-	s.RegisterService(&SearchMovieService_ServiceDesc, srv)
+func RegisterSearchMovieServer(s grpc.ServiceRegistrar, srv SearchMovieServer) {
+	s.RegisterService(&SearchMovie_ServiceDesc, srv)
 }
 
-func _SearchMovieService_SearchMovie_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _SearchMovie_SearchMovie_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(SearchMovieRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(SearchMovieServiceServer).SearchMovie(ctx, in)
+		return srv.(SearchMovieServer).SearchMovie(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/movie.SearchMovieService/SearchMovie",
+		FullMethod: "/movie.SearchMovie/SearchMovie",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(SearchMovieServiceServer).SearchMovie(ctx, req.(*SearchMovieRequest))
+		return srv.(SearchMovieServer).SearchMovie(ctx, req.(*SearchMovieRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _SearchMovieService_GetMovieDetail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _SearchMovie_GetMovieDetail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(GetMovieDetailRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(SearchMovieServiceServer).GetMovieDetail(ctx, in)
+		return srv.(SearchMovieServer).GetMovieDetail(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/movie.SearchMovieService/GetMovieDetail",
+		FullMethod: "/movie.SearchMovie/GetMovieDetail",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(SearchMovieServiceServer).GetMovieDetail(ctx, req.(*GetMovieDetailRequest))
+		return srv.(SearchMovieServer).GetMovieDetail(ctx, req.(*GetMovieDetailRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// SearchMovieService_ServiceDesc is the grpc.ServiceDesc for SearchMovieService service.
+// SearchMovie_ServiceDesc is the grpc.ServiceDesc for SearchMovie service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var SearchMovieService_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "movie.SearchMovieService",
-	HandlerType: (*SearchMovieServiceServer)(nil),
+var SearchMovie_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "movie.SearchMovie",
+	HandlerType: (*SearchMovieServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "SearchMovie",
-			Handler:    _SearchMovieService_SearchMovie_Handler,
+			Handler:    _SearchMovie_SearchMovie_Handler,
 		},
 		{
 			MethodName: "GetMovieDetail",
-			Handler:    _SearchMovieService_GetMovieDetail_Handler,
+			Handler:    _SearchMovie_GetMovieDetail_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
