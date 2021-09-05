aa2b654e8f934e08e748b177ba62dc2895cac214
Albert Zenko <zenkobert@gmail.com>
2021-09-05 10:55:33 +0700

    log error

--- repository/movie.go
+++ repository/movie.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 
 	model "github.com/zenkobert/sbtest-2/domain"
@@ -28,23 +29,29 @@ func NewMovieRepo(apiKey string) model.MovieRepository {
 func (repo *movieRepo) SearchMovies(title string, page uint32) (result *model.MovieSearch, err error) {
 	url := fmt.Sprintf("%s/?apikey=%s&s=%s&page=%d", host, repo.apiKey, title, page)
 	req, err := http.NewRequest(http.MethodGet, url, nil)
+
 	if err != nil {
+		log.Println(err)
 		return result, err
 	}
 
 	resp, err := repo.Client.Do(req)
 	if err != nil || resp == nil {
+		log.Println(err)
 		return result, err
 	}
 	defer resp.Body.Close()
 
 	body, _ := ioutil.ReadAll(resp.Body)
 	if resp.StatusCode >= 400 {
+		log.Println(err)
 		return result, err
 	}
 
+	result = &model.MovieSearch{}
 	err = json.Unmarshal(body, result)
 	if err != nil {
+		log.Println(err)
 		return result, err
 	}
 
@@ -55,20 +62,24 @@ func (repo *movieRepo) GetMovieDetailByID(id string) (detail *model.MovieDetail,
 	url := fmt.Sprintf("%s/?apikey=%s&i=%s", host, repo.apiKey, id)
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
+		log.Println(err)
 		return detail, err
 	}
 
 	resp, err := repo.Client.Do(req)
 	if err != nil || resp == nil {
+		log.Println(err)
 		return detail, err
 	}
 	defer resp.Body.Close()
 
 	body, _ := ioutil.ReadAll(resp.Body)
 	if resp.StatusCode >= 400 {
+		log.Println(err)
 		return detail, err
 	}
 
+	detail = &model.MovieDetail{}
 	err = json.Unmarshal(body, detail)
 	if err != nil {
 		return detail, err
