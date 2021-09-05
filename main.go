b6bdb2df9decedb77f0310fb3356ed1a903b2b94
Albert Zenko <zenkobert@gmail.com>
2021-09-05 11:13:26 +0700

    assemble all pieces

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	server "github.com/zenkobert/sbtest-2/delivery/grpc/movie"
+	repo "github.com/zenkobert/sbtest-2/repository"
+	usecase "github.com/zenkobert/sbtest-2/usecase"
+	"google.golang.org/grpc"
+)
 
 func main() {
-	fmt.Print("asdadada")
+	movieRepo := repo.NewMovieRepo("faf7e5bb")
+	movieUsecase := usecase.NewMovieUsecase(movieRepo)
+	movieServer := server.NewMovieServer(movieUsecase)
+
+	grpcServer := grpc.NewServer()
+	server.RegisterSearchMovieServer(grpcServer, movieServer)
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", "8080"))
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("GRPC Server Started")
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve %v", err)
+	}
 }
